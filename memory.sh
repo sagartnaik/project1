81379236c937e077dcd54dcccc00888d568f37cb
Ubuntu <ubuntu@ip-172-31-16-187.us-west-1.compute.internal>
2022-06-20 16:55:13 +0000

    2 files

---
+++ memory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+space=`df -h . | awk -F " " 'NR==2 { print $5 }' | sed 's/%/ /g'`
+
+if [ $space -ge 26 ]
+then
+	echo "memory reached threashold value" | mail -s "memory utilization" sagar.tn3692@gmail.com
+fi
---
+++ product1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "enter the first number"
+read num1
+echo "enter the second number"
+read num2
+product=`expr $num1 \* $num2`
+echo "product of two numbers is: $product"
+
